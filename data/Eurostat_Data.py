bd99f784edf00dd94e01bdacc907445fc5ff2f3f
Nicolae-01001110 <nistornicolae96@gmail.com>
2024-03-15 15:11:40 +0200

    2023 set

--- data/Eurostat_Data.py
+++ data/Eurostat_Data.py
@@ -10,8 +10,8 @@ client = EurostatAPIClient(VERSION, FORMAT, LANGUAGE)
 gdp_growth_dataset = client.get_dataset('tec00115').to_dataframe()
 GDP_df = gdp_growth_dataset.query("unit == 'CLV_PCH_PRE'")[["time", "geo", "values"]]
 
-with pd.option_context ("display.max_rows", None):
-   print(GDP_df)
-
-# print(GDP_df)
+# with pd.option_context ("display.max_rows", None):
+#    print(GDP_df)
+#
+# # print(GDP_df)
 
